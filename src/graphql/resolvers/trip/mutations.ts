3c1f87f2903118672774a00584c162deedc370d6
DESARROLLOMAPA\desar <cristofhersp@gmail.com>
2024-09-23 18:00:23 -0600

    Cambios flatantes merge rama alan

--- src/graphql/resolvers/trip/mutations.ts
+++ src/graphql/resolvers/trip/mutations.ts
@@ -470,7 +470,24 @@ const tripMutations = {
         } catch (error) {
             console.log(error);
         }
-    }
+    },
+    UPDATE_TOTALWEIGHT_TRIP: async (_: any, {input}: any) => {
+        const {
+            idTrip,
+            userLoggedIn,
+        } = input;
+
+        try {
+            await trip.query("exec update_totalWeight_tripProducts " +
+                "@idTrip = '" + idTrip + "', " +
+                "@userLoggedIn = '" + userLoggedIn + "' "
+            );
+            return 'Peso total de viaje actualizado correctamente'
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
 };
 
 export default tripMutations;
