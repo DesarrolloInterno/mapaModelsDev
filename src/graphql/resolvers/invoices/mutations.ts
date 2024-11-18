3ed1588a4b5d05b41e0a948e48163a9b2138bab8
DESARROLLOMAPA\desar <cristofhersp@gmail.com>
2024-11-18 16:11:10 -0600

    Cambios en facturación

--- src/graphql/resolvers/invoices/mutations.ts
+++ src/graphql/resolvers/invoices/mutations.ts
@@ -62,6 +62,7 @@ const invoicesMutations = {
             idInvoice,
             linkPDF,
             linkXML,
+            linkInvoicePDF,
             userLoggedIn,
         } = input;
 
@@ -70,6 +71,7 @@ const invoicesMutations = {
                 "@idInvoice = '" + idInvoice + "', " +
                 "@linkPDF = '" + linkPDF + "', " +
                 "@linkXML = '" + linkXML + "', " +
+                "@linkInvoicePDF = '" + linkInvoicePDF + "', " +
                 "@userLoggedIn = '" + userLoggedIn + "' "
             );
             return 'Documentos de factura actualizados correctamente'
@@ -77,6 +79,128 @@ const invoicesMutations = {
             console.log(error);
         }
 
+    },
+    CREATE_INVOICE: async (_: any, {input}: any) => {
+        const {
+            idTrip,
+            idUseOfCFDI,
+            idPaymentMethod,
+            idPayment,
+            currency,
+            exchangeRate,
+            userLoggedIn,
+        } = input;
+
+        try {
+            const newid = await invoices.query("exec create_invoice " +
+                "@idTrip = '" + idTrip + "', " +
+                "@idUseOfCFDI = '" + idUseOfCFDI + "', " +
+                "@idPaymentMethod = '" + idPaymentMethod + "', " +
+                "@idPayment = '" + idPayment + "', " +
+                "@currency = '" + currency + "', " +
+                "@exchangeRate = '" + exchangeRate + "', " +
+                "@userLoggedIn = '" + userLoggedIn + "' "
+            );
+            return newid;
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
+    SET_INVOICE_AMOUNTS: async (_: any, {input}: any) => {
+        const {
+            idInvoiceAmount,
+            idInvoice,
+            idTrip,
+            idSalesFormatAmounts,
+            idInvoicingConcept,
+            subtotal,
+            idIVApercentage,
+            IVApercentage,
+            IVA,
+            idRETpercentage,
+            RETpercentage,
+            RET,
+            totalAmount,
+            idSalesFormat,
+            codeMeasuringUnit,
+            measuringUnit,
+            isTaxes,
+            userLoggedIn,
+            status
+        } = input;
+
+        try {
+            await invoices.query("exec set_invoiceAmounts " +
+                "@idInvoiceAmount = '" + idInvoiceAmount + "', " +
+                "@idInvoice = '" + idInvoice + "', " +
+                "@idTrip = '" + idTrip + "', " +
+                "@idSalesFormatAmounts = '" + idSalesFormatAmounts + "', " +
+                "@idInvoicingConcept = '" + idInvoicingConcept + "', " +
+                "@subtotal = '" + subtotal + "', " +
+                "@idIVApercentage = '" + idIVApercentage + "', " +
+                "@IVApercentage = '" + IVApercentage + "', " +
+                "@IVA = '" + IVA + "', " +
+                "@idRETpercentage = '" + idRETpercentage + "', " +
+                "@RETpercentage = '" + RETpercentage + "', " +
+                "@RET = '" + RET + "', " +
+                "@totalAmount = '" + totalAmount + "', " +
+                "@idSalesFormat = '" + idSalesFormat + "', " +
+                "@codeMeasuringUnit = '" + codeMeasuringUnit + "', " +
+                "@measuringUnit = '" + measuringUnit + "', " +
+                "@isTaxes = '" + isTaxes + "', " +
+                "@userLoggedIn = '" + userLoggedIn + "', " +
+                "@status = '" + status + "' "
+            );
+            return 'Montos de factura actualizados correctamente'
+        } catch (error) {
+            console.log(error);
+        }
+
+    },
+    UPDATE_INVOICE: async (_: any, {input}: any) => {
+        const {
+            idInvoice,
+            idTrip,
+            idUseOfCFDI,
+            idPaymentMethod,
+            idPayment,
+            currency,
+            exchangeRate,
+            userLoggedIn,
+        } = input;
+
+        try {
+            const newid = await invoices.query("exec update_invoice " +
+                "@idInvoice = '" + idInvoice + "', " +
+                "@idTrip = '" + idTrip + "', " +
+                "@idUseOfCFDI = '" + idUseOfCFDI + "', " +
+                "@idPaymentMethod = '" + idPaymentMethod + "', " +
+                "@idPayment = '" + idPayment + "', " +
+                "@currency = '" + currency + "', " +
+                "@exchangeRate = '" + exchangeRate + "', " +
+                "@userLoggedIn = '" + userLoggedIn + "' "
+            );
+            return newid;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    SET_TOKEN_STAMPINVOICE: async (_: any, {input}: any) => {
+        const {
+            idInvoice,
+            userLoggedIn,
+        } = input;
+
+        try {
+            const newid = await invoices.query("exec set_token_stampInvoice " +
+                "@idInvoice = '" + idInvoice + "', " +
+                "@userLoggedIn = '" + userLoggedIn + "' "
+            );
+            return newid;
+        } catch (error) {
+            console.log(error);
+        }
     },
 };
 
