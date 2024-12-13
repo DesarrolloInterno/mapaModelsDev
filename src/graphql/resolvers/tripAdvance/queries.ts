4d75dcd637b247c4af5a3a369de2bb050f40911d
AuxDesarrolloMapa <auxdesarrollointernomapa@gmail.com>
2024-12-13 10:47:44 -0600

    Apartado reporte anticipo de viaje

--- src/graphql/resolvers/tripAdvance/queries.ts
+++ src/graphql/resolvers/tripAdvance/queries.ts
@@ -9,6 +9,33 @@ const tripAdvanceQueries = {
         } catch (error) {
             console.log(error);
         }
+    },
+    GET_TRIPADVANCES: async (_: any, {date1, date2, idEnterprise, typeFilter}: any) => {
+        try{
+            const result = await tripAdvance.query("exec get_tripAdvances @date1 = '"+date1+"', @date2 = '"+date2+"', @idEnterprise = '"+ idEnterprise +"', @typeFilter = '"+ typeFilter + "'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    GET_TRIPADVANCES_BY_IDTRIP: async (_: any, {idTrip, idEnterprise}: any) => {
+        try{
+            const result = await tripAdvance.query("exec get_all_tripAdvances_by_idTrip @idTrip = '"+idTrip+"', @idEnterprise = '"+ idEnterprise +"'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    GET_TRIPADVANCES_FOR_CROSSING_TRIPS: async (_: any, {date1, date2, idEnterprise, typeFilter}: any) => {
+        try{
+            const result = await tripAdvance.query("exec get_tripAdvances_for_crossingTrips @date1 = '"+date1+"', @date2 = '"+date2+"', @idEnterprise = '"+ idEnterprise +"', @typeFilter = '"+ typeFilter + "'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
 
