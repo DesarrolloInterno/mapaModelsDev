bdce7f6704491a6656c202e2dca2b9eab8154e46
AuxDesarrolloMapa <auxdesarrollointernomapa@gmail.com>
2025-03-13 13:20:57 -0600

    Apartado de pólizas

--- src/graphql/resolvers/vouchers/mutations.ts
+++ src/graphql/resolvers/vouchers/mutations.ts
@@ -3,15 +3,110 @@ import { vouchers } from "../../../db/Entities/vouchers";
 const vouchersMutations = {
     CREATE_VOUCHER: async (_: any, {input}: any) => {
         const { 
-            name,
+            idBeneficiary,
+            beneficiaryType,
+            voucherDate,
+            voucherType,
+            idOrigin,
+            origin,
+            description,
             userLoggedIn,
-            idEnterprise
+            idEnterprise,
+            beneficiaryName
         } = input;
         try {
             const newRecord = await vouchers.query("exec create_voucher " +
-                "@name = '"+name+"', " +
+                "@idBeneficiary = '"+idBeneficiary+"', " +
+                "@beneficiaryType = '"+beneficiaryType+"', " +
+                "@voucherDate = '"+voucherDate+"', " +
+                "@voucherType = '"+voucherType+"', " +
+                "@idOrigin = '"+idOrigin+"', " +
+                "@origin = '"+origin+"', " +
+                "@description = '"+description+"', " +
                 "@userLoggedIn = '"+userLoggedIn+"', " +
-                "@idEnterprise = '"+idEnterprise+"'");
+                "@idEnterprise = '"+idEnterprise+"', " +
+                "@beneficiaryName = '"+beneficiaryName+"'");
+            return newRecord;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    UPDATE_VOUCHER: async (_: any, {input}: any) => {
+        const { 
+            idVoucher,
+            idBeneficiary,
+            beneficiaryType,
+            voucherDate,
+            voucherType,
+            idOrigin,
+            origin,
+            description,
+            userLoggedIn,
+            idEnterprise,
+            beneficiaryName
+        } = input;
+        try {
+            const newRecord = await vouchers.query("exec update_voucher " +
+                "@idVoucher = '"+idVoucher+"', " +
+                "@idBeneficiary = '"+idBeneficiary+"', " +
+                "@beneficiaryType = '"+beneficiaryType+"', " +
+                "@voucherDate = '"+voucherDate+"', " +
+                "@voucherType = '"+voucherType+"', " +
+                "@idOrigin = '"+idOrigin+"', " +
+                "@origin = '"+origin+"', " +
+                "@description = '"+description+"', " +
+                "@userLoggedIn = '"+userLoggedIn+"', " +
+                "@idEnterprise = '"+idEnterprise+"', " +
+                "@beneficiaryName = '"+beneficiaryName+"'");
+            return newRecord;
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    DELETE_VOUCHER: async (_: any, {input}: any) => {
+        const { 
+            idVoucher,
+            userLoggedIn
+        } = input;
+        try {
+            await vouchers.query("exec delete_voucher " +
+                "@idVoucher = '"+idVoucher+"', " +
+                "@userLoggedIn = '"+userLoggedIn+"'"
+            );
+            return "Póliza eliminada correctamente";
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    UPDATE_VOUCHER_DETAIL: async (_: any, {input}: any) => {
+        const { 
+            idVoucherDetail,
+            idVoucher,
+            idAccountingAccount,
+            accountingAccount,
+            accountingAccountName,
+            reference,
+            description,
+            charges,
+            payments,
+            idEnterprise,
+            userLoggedIn,
+            status
+        } = input;
+        try {
+            const newRecord = await vouchers.query("exec update_voucherDetail " +
+                "@idVoucherDetail = '"+idVoucherDetail+"', " +
+                "@idVoucher = '"+idVoucher+"', " +
+                "@idAccountingAccount = '"+idAccountingAccount+"', " +
+                "@accountingAccount = '"+accountingAccount+"', " +
+                "@accountingAccountName = '"+accountingAccountName+"', " +
+                "@reference = '"+reference+"', " +
+                "@description = '"+description+"', " +
+                "@charges = '"+charges+"', " +
+                "@payments = '"+payments+"', " +
+                "@idEnterprise = '"+idEnterprise+"', " +
+                "@status = '"+status+"', " +
+                "@userLoggedIn = '"+userLoggedIn+"'");
             return newRecord;
         } catch (error) {
             console.log(error);
--- src/graphql/resolvers/vouchers/queries.ts
+++ src/graphql/resolvers/vouchers/queries.ts
@@ -9,6 +9,24 @@ const vouchersQueries = {
         } catch (error) {
             console.log(error);
         }
+    },
+    GET_ALL_VOUCHER_BY_ID: async (_: any, {idVoucher, idEnterprise}: any) => {
+        try{
+            const result = await vouchers.query("exec get_all_voucher_by_id @idVoucher = '"+ idVoucher +"', @idEnterprise = '"+ idEnterprise +"'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    GET_ALL_VOUCHER_DETAIL_BY_IDVOUCHER: async (_: any, {idVoucher, idEnterprise}: any) => {
+        try{
+            const result = await vouchers.query("exec get_all_voucherDetail_by_idVoucher @idVoucher = '"+ idVoucher +"', @idEnterprise = '"+ idEnterprise +"'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
 
