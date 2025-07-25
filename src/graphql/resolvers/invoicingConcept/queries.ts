4aef4d9cc58f79130e915ee79bec499277745c64
desarrollointernomapa <desarrollointernomapa@gmail.com>
2025-07-25 12:18:51 -0600

    cambios conceptos facturación

--- src/graphql/resolvers/invoicingConcept/queries.ts
+++ src/graphql/resolvers/invoicingConcept/queries.ts
@@ -11,6 +11,15 @@ const invoicingConceptQueries = {
             console.log(error);
         }
     },
+    GET_INVOICINGCONCEPT2: async (_: any, {status, idEnterprise}: any) => {
+        try{
+            const result = await invoicingConcept.query("exec get_all_invoicingConcept2 @status = '"+ status +"', @idEnterprise = '"+ idEnterprise +"'");
+            return result;
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
     GET_INVOICINGCONCEPTS_ISNOT_OTHERINCOME: async (_: any, {idEnterprise}: any) => {
         try{
             const result = await invoicingConcept.query("exec get_all_invoicingConcepts_isNot_otherIncome @idEnterprise = '"+ idEnterprise +"'");
